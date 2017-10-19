8b27f1c8af5d0f448f2c45a445c13b53c18495e7
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-19 16:47:18 +0100

    move actualy testing of connection to lambda out of main tests

--- src/test/scala/com/gu/acquisitionsValueCalculatorClient/services/AnnualisedValueTest.scala
+++ src/test/scala/com/gu/acquisitionsValueCalculatorClient/services/AnnualisedValueTest.scala
@@ -1,19 +1,12 @@
 package com.gu.acquisitionsValueCalculatorClient.services
 
-import com.gu.acquisitionsValueCalculatorClient.model.{AcquisitionModel, AnnualisedValueTwo}
+import com.gu.acquisitionsValueCalculatorClient.model.{AcquisitionModel}
 import com.gu.acquisitionsValueCalculatorClient.service.AnnualisedValueService
 import ophan.thrift.event.{PaymentFrequency, PaymentProvider, Product}
 import org.scalatest._
 import org.scalatest.concurrent.Eventually
 
 class AnnualisedValueTest extends FlatSpec with Matchers with OptionValues with Eventually {
-
-  behavior of "AV service"
-
-  it should "succesfully get AV from correct submission" in {
-    val acquisition = AcquisitionModel(50, Product(1), "GBP", PaymentFrequency(1), Some(PaymentProvider(1)))
-    AnnualisedValueService.getAV(acquisition, "ophan") should be ('right)
-  }
 
   behavior of "from Primitives"
 
