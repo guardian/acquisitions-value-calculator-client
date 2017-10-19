f009177a6d7eed0de5295b4e5c8e35f6f789f380
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-19 09:25:21 +0100

    create constructor for AcquisitionModel thaat takes primitive types

--- src/main/scala/com/gu/acquisitionsValueCalculatorClient/model/AcquisitionModel.scala
+++ src/main/scala/com/gu/acquisitionsValueCalculatorClient/model/AcquisitionModel.scala
@@ -4,8 +4,17 @@ import com.gu.fezziwig.CirceScroogeMacros._
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto._
 import ophan.thrift.event._
+import io.circe.syntax._
+import io.circe.parser._
+import io.circe.Error
+
 
 case class AcquisitionModel(amount: Double, product: Product, currency: String, paymentFrequency: PaymentFrequency, paymentProvider: Option[PaymentProvider])
+case class AcquisitionModelFromPrimitives(amount: Double, product: String, currency: String, paymentFrequency: String, paymentProvider: Option[String])
+object AcquisitionModelFromPrimitives {
+  implicit val acquisitionModelFromPrimitivesDecoder: Encoder[AcquisitionModelFromPrimitives] = deriveEncoder
+}
+
 
 object AcquisitionModel {
   implicit val acquisitionEncode: Encoder[AcquisitionModel] = {
@@ -20,6 +29,11 @@ object AcquisitionModel {
     implicit val paymentProviderDecoder: Decoder[PaymentProvider] = decodeThriftEnum
     implicit val productDecoder: Decoder[Product] = decodeThriftEnum
     deriveDecoder
+  }
+
+  def fromPrimitives(amount: Double, product: String, currency: String, paymentFrequency: String, paymentProvider: Option[String]): Either[Error, AcquisitionModel] = {
+    val acquistionFromPrimitives  = AcquisitionModelFromPrimitives(amount, product, currency, paymentFrequency, paymentProvider).asJson.noSpaces
+    decode[AcquisitionModel](acquistionFromPrimitives)
   }
 
 }
