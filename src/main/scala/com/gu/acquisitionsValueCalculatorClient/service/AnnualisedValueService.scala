45bb04e774f308e6e65499f7f7db98d6a752787a
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-20 16:40:28 +0100

    not working

--- src/main/scala/com/gu/acquisitionsValueCalculatorClient/service/AnnualisedValueService.scala
+++ src/main/scala/com/gu/acquisitionsValueCalculatorClient/service/AnnualisedValueService.scala
@@ -3,7 +3,7 @@ package com.gu.acquisitionsValueCalculatorClient.service
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.lambda.model.InvokeRequest
-import com.gu.acquisitionsValueCalculatorClient.model.{AcquisitionModel, AnnualisedValueResult}
+import com.gu.acquisitionsValueCalculatorClient.model.{AVError, AcquisitionModel, AnnualisedValueResult, AnnualisedValueTwo}
 import io.circe.syntax._
 import io.circe.parser._
 import cats.syntax.either._
@@ -17,7 +17,7 @@ object AnnualisedValueService {
     decode[AnnualisedValueResult](json).leftMap(e => "Error: Unable to parse ( " + json + "). " + e.getMessage)
   }
 
-  def getAV(acquisitionModel: AcquisitionModel, accountName: String): Either[String, AnnualisedValueResult] = {
+  def getAV(acquisitionModel: AcquisitionModel, accountName: String): Either[String, Double] = {
 
     implicit val region: Regions = AnnualisedValueClient.getRegion
     implicit val lambda = AnnualisedValueClient.createLambdaClient(region, new ProfileCredentialsProvider(accountName))
@@ -26,9 +26,10 @@ object AnnualisedValueService {
     invokeRequest.setFunctionName("acquisitions-value-calculator-PROD")
     invokeRequest.setPayload(acquisitionModel.asJson.noSpaces)
     val response = new String(lambda.invoke(invokeRequest).getPayload.array())
-    annualisedValueResultFromJson(response)
+    annualisedValueResultFromJson(response).flatMap{
+      case AVError(e) => Left(e)
+      case AnnualisedValueTwo(amount) => Right(amount)
+    }
 
   }
-
-
 }
