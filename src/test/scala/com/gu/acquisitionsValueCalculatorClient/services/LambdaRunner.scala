91fa17d92f8dc470aa6a1807f2d39390fdf3fd2d
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-11-16 18:16:22 +0000

    fix model (#9)

--- src/test/scala/com/gu/acquisitionsValueCalculatorClient/services/LambdaRunner.scala
+++ src/test/scala/com/gu/acquisitionsValueCalculatorClient/services/LambdaRunner.scala
@@ -8,5 +8,5 @@ import org.scalatest.{Matchers, OptionValues}
 //For testing locally that the lambda is being called correctly. Run with "sbt test:run"
 object LambdaRunner extends App with Matchers with OptionValues with Eventually {
   val acquisition = AcquisitionModel(50, "CONTRIBUTION", "GBP", "ONE_OFF", Some("STRIPE"), Some(PrintOptionsModel("VOUCHER_WEEKEND_PLUS", "GB")))
-  AnnualisedValueService.getAV(acquisition, "ophan") should be ('right)
+  AnnualisedValueService.getAV(acquisition, "ophan") shouldBe ('right)
 }
