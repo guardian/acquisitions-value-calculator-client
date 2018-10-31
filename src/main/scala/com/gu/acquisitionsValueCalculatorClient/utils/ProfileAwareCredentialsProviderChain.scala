11327046abf1d53f0a4dc3b057bb16907036dfa2
rupertbates <rupert.bates@guardian.co.uk>
2018-10-31 12:04:06 +0000

    Setting version to 2.0.4-SNAPSHOT

--- src/main/scala/com/gu/acquisitionsValueCalculatorClient/utils/ProfileAwareCredentialsProviderChain.scala
+++ src/main/scala/com/gu/acquisitionsValueCalculatorClient/utils/ProfileAwareCredentialsProviderChain.scala
@@ -10,7 +10,9 @@ case class ProfileAwareCredentialsProviderChain(profile: String) extends AWSCred
     new EnvironmentVariableCredentialsProvider,
     new SystemPropertiesCredentialsProvider,
     profileCredentialsProvider,
-    new InstanceProfileCredentialsProvider(false))
+    new InstanceProfileCredentialsProvider(false),
+    new STSAssumeRoleSessionCredentialsProvider.Builder("arn:aws:iam::021353022223:role/support-invoke-value-calculator", "invokeLambda").build()
+  )
 
   override def refresh(): Unit = chain.refresh()
   override def getCredentials: AWSCredentials = chain.getCredentials
